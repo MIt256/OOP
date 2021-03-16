02051024e473ecd1f4eec4e825fc4f59191a96e7
Maks <xxzanko@gmail.com>
2021-03-16 21:18:50 +0300

    last1

--- src/main/java/org/example/Shapes/Ellipse.java
+++ src/main/java/org/example/Shapes/Ellipse.java
@@ -4,12 +4,11 @@ import javafx.scene.paint.Color;
 
 public class Ellipse extends Shape {
     //конструктор
-    public Ellipse(Color brushColor,Color fillColor, int brushSize,int x1,int y1) {
+    public Ellipse(Color brushColor,Color fillColor, int brushSize) {
         this.brushColor = brushColor;
         this.brushSize = brushSize;
         this.fillColor = fillColor;
-        this.x1 = x1;
-        this.y1 = y1;
+
     }
 
     @Override
